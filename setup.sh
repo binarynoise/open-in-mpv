a685bc034b8fc7a6765cbf7dbd7013da6fa2c9aa
binarynoise <binarynoise@mail.de>
2023-07-22 23:20:26 +0200

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # one last thing to do: to use the extension, run the following commands in your terminal:
 
-curl -sSf "https://raw.githubusercontent.com/binarynoise/open-in-mpv/main/mpv-scheme-handler.desktop" >~/.local/share/applications/mpv-scheme-handler.desktop &&
-    xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
+set -e
+
+if [ -f mpv-scheme-handler.desktop ]; then
+    cp mpv-scheme-handler.desktop ~/.local/share/applications/mpv-scheme-handler.desktop
+else
+    curl -sSf "https://raw.githubusercontent.com/binarynoise/open-in-mpv/main/mpv-scheme-handler.desktop" >~/.local/share/applications/mpv-scheme-handler.desktop
+fi
+
+xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
